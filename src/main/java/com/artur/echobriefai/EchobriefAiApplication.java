697107df4739efcbfcc1ece00e38e5c995799527
ArturCesarGit <168553261+ArturCesarGit@users.noreply.github.com>
2026-05-26 00:51:23 -0300

    projeto inicializado

---
+++ src/main/java/com/artur/echobriefai/EchobriefAiApplication.java
@@ -0,0 +1,13 @@
+package com.artur.echobriefai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchobriefAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EchobriefAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/artur/echobriefai/EchobriefAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artur.echobriefai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchobriefAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
